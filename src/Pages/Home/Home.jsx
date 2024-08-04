7a3dd75ec9dd52fb3b47a94fcd28c4509f66d159
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-08-04 20:42:45 +0600

    load dynamic data and show them in the website

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,66 @@
+import Slider from "react-slick";
 import img1 from "../../../public/homeImg1.png";
 import img2 from "../../../public/homeImg2.png";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './therapist.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+       var settings = {
+              dots: true,
+              infinite: true,
+              speed: 500,
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              initialSlide: 0,
+              autoplay: true,
+              autoplaySpeed: 2000,
+              cssEase: "linear",
+              responsive: [
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    initialSlide: 3,
+                    infinite: true,
+                    dots: true,
+                  },
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                  },
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                  },
+                },
+              ],
+            };
+          
+            const [therapist, setTherapist] = useState([]);
+          
+            useEffect(() => {
+              const fetchData = async () => {
+                const response = await axios.get(
+                  "../../../public/therapist.json"
+                );
+                // console.log(response.data);
+                setTherapist(response.data);
+              };
+          
+              fetchData();
+            }, []);
+
 return (
 <div>
    <div className="flex bg-white m-5 rounded p-2">
@@ -25,6 +84,22 @@ return (
 
    <div className="m-5">
        <h1 className="font-[Poppins]">Featured Therapist</h1>
+       <div className="w-[85%] m-auto">
+        <Slider {...settings} >
+          {therapist.map((data) => (
+            <>
+              <div className="bg-white mx-2 my-10  h-[350px] rounded-xl max-w-[350px] border-[1px] ">
+                <img src={data.image} alt="" className="w-full h-[200px] p-5" />
+                <div className="flex flex-col justify-center items-center gap-4 p-2">
+                  <h1 className="font-bold text-xl">{data.therapistName}</h1>
+                  <h1 className="font-bold text-[14px] text-blue-800">{data.city}{data.country}</h1>
+                  <p className="text-sm">{data.chamber}</p>
+                </div>
+              </div>
+            </>
+          ))}
+        </Slider>
+        </div>
    </div>                                           
 </div>
 );
